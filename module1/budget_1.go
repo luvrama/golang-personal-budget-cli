7b804984530189fc128918a8207560ad2d879168
RAMACHANDRAN ELUMALAI <luvrama@ramachadransair.lan>
2019-12-12 04:47:02 -0700

    s

--- module1/budget_1.go
+++ module1/budget_1.go
@@ -1,10 +1,11 @@
 package module1
 
 // Budget stores budget information
-tye Budget struct {
+type Budget struct {
 	Max float32
 	Items []Item
 }
+
 // Item stores item information
 type Item struct {
 	Description string
